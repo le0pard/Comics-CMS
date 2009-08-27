7671636d00db11a769ed493889f259fb8d6f06bf
leo <leo@leo-laptop>
2009-08-28 00:53:17 +0300

    Fix js

--- public/script.js
+++ public/script.js
@@ -5,18 +5,30 @@
 var ComicsObj = {
 	init: function(){
 		document.observe('keydown', function(event){
-			if (event.ctrlKey) {
-				if (event.keyCode == Event.KEY_LEFT) {
+			var evt = event || window.event;
+			var key = evt.keyCode || evt.which;
+			
+			if (evt.altKey) {
+				if ('function' == typeof evt.preventDefault){
+					evt.preventDefault();
+				} else {
+					evt.returnValue = false;
+				}
+				
+				if (key == Event.KEY_LEFT) {
 					if ($('previous_comics')){
 						location.href = $('previous_comics').href;
 					}
 				}
 				else 
-					if (event.keyCode == Event.KEY_RIGHT) {
+					if (key == Event.KEY_RIGHT) {
 						if ($('next_comics')){
 							location.href = $('next_comics').href;
 						}
 					}
+					
+				window.focus();
+				return false;
 			}
 		});
 	}
