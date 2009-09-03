0a3e50431f507cde872034a6e5da6d5e0b5064bf
Alexei <leopard.not.a@gmail.com>
2009-09-03 23:47:55 +0300

    fix hotkey

--- public/script.js
+++ public/script.js
@@ -9,23 +9,30 @@ var ComicsObj = {
 			var key = evt.keyCode || evt.which;
 			//not work in Opera
 			if (evt.ctrlKey) {
-				if ('function' == typeof evt.preventDefault){
-					evt.preventDefault();
-				} else {
-					evt.returnValue = false;
-				}
 				
+				var keySt = false;
 				if (key == Event.KEY_LEFT) {
 					if ($('previous_comics')){
 						location.href = $('previous_comics').href;
+						keySt = true;
 					}
 				}
 				else 
 					if (key == Event.KEY_RIGHT) {
 						if ($('next_comics')){
 							location.href = $('next_comics').href;
+							keySt = true;
 						}
 					}
+					
+				if (keySt) {
+					if ('function' == typeof evt.preventDefault) {
+						evt.preventDefault();
+					}
+					else {
+						evt.returnValue = false;
+					}
+				}
 					
 				window.focus();
 				return false;
